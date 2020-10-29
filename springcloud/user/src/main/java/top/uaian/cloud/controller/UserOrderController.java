a99a125bb576980d55db3b5bac55969ef5974e87
xukainan <xukainan@fang.com>
2020-10-29 16:22:46 +0800

    springcloud-bus动态刷新配置实现

--- springcloud/user/src/main/java/top/uaian/cloud/controller/UserOrderController.java
+++ springcloud/user/src/main/java/top/uaian/cloud/controller/UserOrderController.java
@@ -6,6 +6,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 //@DefaultProperties(defaultFallback = "defaultFallbackMethod")
 public class UserOrderController {
 
