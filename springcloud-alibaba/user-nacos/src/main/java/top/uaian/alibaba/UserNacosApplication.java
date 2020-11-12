17b927e81e7bb5635c4f3b76caba47719173b929
xukainan <xukainan@fang.com>
2020-11-12 14:12:00 +0800

    Nacos 生产者消费者

---
+++ springcloud-alibaba/user-nacos/src/main/java/top/uaian/alibaba/UserNacosApplication.java
@@ -0,0 +1,25 @@
+package top.uaian.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserNacosApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
