cf948ae58c87df4e5916e298802ea44a2503bab6
xukainan <xukainan@fang.com>
2020-11-13 15:10:16 +0800

    Sentinel 流控降级

--- springcloud-alibaba/user-nacos/src/main/java/top/uaian/alibaba/controller/UserController.java
+++ springcloud-alibaba/user-nacos/src/main/java/top/uaian/alibaba/controller/UserController.java
@@ -1,5 +1,8 @@
 package top.uaian.alibaba.controller;
 
+import com.alibaba.csp.sentinel.adapter.spring.webflux.exception.SentinelBlockExceptionHandler;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -27,5 +30,22 @@ public class UserController {
     public String getNacosOrder(@RequestParam Integer id){
         String forObject = restTemplate.getForObject(service + "/getNacosOrder?id=" + id, String.class);
         return forObject;
+    }
+
+
+    @GetMapping("/getSentinel")
+    @SentinelResource(value = "getSentinel", blockHandler = "dealSentinelHandler", fallback = "fallback",
+            exceptionsToIgnore = {InterruptedException.class})
+    public String getSentinbel(){
+        return "getSentinel";
+    }
+
+    public String dealSentinelHandler(BlockException ex){
+        return "dealSentinelHandler:" + ex.getMessage();
+    }
+
+
+    public String fallback(Throwable throwable){
+        return "fallback:" + throwable.getMessage();
     }
 }
